ea8bb0c552d7d1045fa81c740fda0323d65aded1
David Couronné <couronne.david@gmail.com>
2019-11-09 19:33:35 +0100

    Initial Commit

---
+++ gridsome.config.js
@@ -0,0 +1,75 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+const tailwind = require('tailwindcss')
+const purgecss = require('@fullhuman/postcss-purgecss')
+
+const postcssPlugins = [tailwind()]
+
+if (process.env.NODE_ENV === 'production') postcssPlugins.push(purgecss())
+
+module.exports = {
+  siteName: 'David Couronné',
+  siteDescription:
+    'Portfolio de David Couronné powerd by Gridsome and Tailwind CSS',
+  siteUrl: 'https://gridsome-portfolio-starter.netlify.com',
+  plugins: [
+    {
+      use: '@gridsome/vue-remark',
+      options: {
+        typeName: 'Documentation', // Required
+        baseDir: './docs', // Where .md files are located
+        pathPrefix: '/docs', // Add route prefix. Optional
+        template: './src/templates/Documentation.vue', // Optional
+        plugins: [
+          
+        ]
+      }
+    },
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'blog/**/*.md',
+        typeName: 'Post',
+        refs: {
+          tags: {
+            typeName: 'Tag',
+            create: true
+          }
+        }
+      }
+    },
+    
+    {
+      use: '@gridsome/plugin-sitemap',
+      options: {
+        cacheTime: 600000 // default
+      }
+    }
+  ],
+  templates: {
+    Tag: '/tag/:id'
+  },
+  transformers: {
+    remark: {
+      plugins: [
+        [
+         
+        ]
+      ],
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      anchorClassName: 'icon icon-link'
+    }
+  },
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: postcssPlugins
+      }
+    }
+  }
+}
