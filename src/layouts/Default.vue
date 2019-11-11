10aa666276ad96e0a5dc0d1484f611528116ed73
David Couronné <couronne.david@gmail.com>
2019-11-11 19:29:03 +0100

    styles(): add footer

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -110,6 +110,48 @@
 
     
           <slot />
+
+          <!-- footer -->
+          <v-footer
+    dark
+    padless
+  >
+    <v-card
+      class="flex"
+      flat
+      tile
+       color="black"
+    >
+      <v-card-title>
+        <strong class="subheading">Get connected with me on social networks!</strong>
+
+        <v-spacer></v-spacer>
+
+        <v-btn
+         href="https://twitter.com/nollan94"
+          class="mx-4"
+          dark
+          icon
+          title="twitter"
+        >
+          <v-icon size="24px">mdi-twitter</v-icon>
+        </v-btn>
+        <v-btn
+         href="https://github.com/DavidCouronne"
+          class="mx-4"
+          dark
+          icon
+          title="github"
+        >
+          <v-icon size="24px">mdi-github-circle</v-icon>
+        </v-btn>
+      </v-card-title>
+
+      <v-card-text class="py-2 white--text text-center">
+        {{ new Date().getFullYear() }} — <strong>David Couronné</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
         
   </v-app>
 </template>
