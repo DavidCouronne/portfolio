7689297638e8a29854dc866ee517a540cac9e246
David Couronné <couronne.david@gmail.com>
2019-11-11 20:01:37 +0100

    chore(): add ./src/layout/partials/Footer.vue

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -10,12 +10,8 @@
       <v-list dense nav>
         <v-list-item>
           <v-list-item-content>
-            <v-list-item-title class="title">
-              David
-            </v-list-item-title>
-            <v-list-item-subtitle>
-              Couronné
-            </v-list-item-subtitle>
+            <v-list-item-title class="title">David</v-list-item-title>
+            <v-list-item-subtitle>Couronné</v-list-item-subtitle>
           </v-list-item-content>
         </v-list-item>
 
@@ -49,68 +45,29 @@
       <v-spacer />
 
       <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn :to="$url('/')">
-          Home
-        </v-btn>
-        <v-btn :to="$url('/blog')">
-          Blog
-        </v-btn>
-        <v-btn :to="$url('/docs')">
-          Documentation
-        </v-btn>
+        <v-btn :to="$url('/')">Home</v-btn>
+        <v-btn :to="$url('/blog')">Blog</v-btn>
+        <v-btn :to="$url('/docs')">Documentation</v-btn>
       </v-toolbar-items>
       <v-btn icon @click="goDark = !goDark">
-        <v-icon v-if="goDark">
-          mdi-white-balance-sunny
-        </v-icon>
-        <v-icon v-else>
-          mdi-moon-waxing-crescent
-        </v-icon>
+        <v-icon v-if="goDark">mdi-white-balance-sunny</v-icon>
+        <v-icon v-else>mdi-moon-waxing-crescent</v-icon>
       </v-btn>
     </v-app-bar>
 
     <slot />
 
     <!-- footer -->
-    <v-footer dark padless>
-      <v-card class="flex" flat tile color="black">
-        <v-card-title>
-          <strong class="subheading"
-            >Get connected with me on social networks!</strong
-          >
-
-          <v-spacer></v-spacer>
-
-          <v-btn
-            href="https://twitter.com/nollan94"
-            class="mx-4"
-            dark
-            icon
-            title="twitter"
-          >
-            <v-icon size="24px">mdi-twitter</v-icon>
-          </v-btn>
-          <v-btn
-            href="https://github.com/DavidCouronne"
-            class="mx-4"
-            dark
-            icon
-            title="github"
-          >
-            <v-icon size="24px">mdi-github-circle</v-icon>
-          </v-btn>
-        </v-card-title>
-
-        <v-card-text class="py-2 white--text text-center">
-          {{ new Date().getFullYear() }} — <strong>David Couronné</strong>
-        </v-card-text>
-      </v-card>
-    </v-footer>
+    <Footer />
   </v-app>
 </template>
 
 <script>
+import Footer from './Partials/Footer'
 export default {
+  components:{
+    Footer
+  },
   data() {
     return {
       goDark: true,
--- src/layouts/Partials/Footer.vue
+++ src/layouts/Partials/Footer.vue
@@ -1,119 +1,23 @@
 <template>
-  <footer class="bg-white">
-    <div class="container mx-auto  px-8">
-      <div class="w-full flex flex-col md:flex-row py-6">
-        <div class="flex-1 mb-6">
-          <a
-            class="text-orange-600 no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
-            href="#"
-          >
-            <!--Icon from: http://www.potlabicons.com/ -->
-            LANDING
-          </a>
-        </div>
+  <v-footer dark padless>
+    <v-card class="flex" flat tile color="black">
+      <v-card-title>
+        <strong class="subheading">Get connected with me!</strong>
 
-        <div class="flex-1">
-          <p class="uppercase text-gray-500 md:mb-6">Links</p>
-          <ul class="list-reset mb-6">
-            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-              <a
-                href="#"
-                class="no-underline hover:underline text-gray-800 hover:text-orange-500"
-                >FAQ</a
-              >
-            </li>
-            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-              <a
-                href="#"
-                class="no-underline hover:underline text-gray-800 hover:text-orange-500"
-                >Help</a
-              >
-            </li>
-            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-              <a
-                href="#"
-                class="no-underline hover:underline text-gray-800 hover:text-orange-500"
-                >Support</a
-              >
-            </li>
-          </ul>
-        </div>
-        <div class="flex-1">
-          <p class="uppercase text-gray-500 md:mb-6">Legal</p>
-          <ul class="list-reset mb-6">
-            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-              <a
-                href="#"
-                class="no-underline hover:underline text-gray-800 hover:text-orange-500"
-                >Terms</a
-              >
-            </li>
-            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-              <a
-                href="#"
-                class="no-underline hover:underline text-gray-800 hover:text-orange-500"
-                >Privacy</a
-              >
-            </li>
-          </ul>
-        </div>
-        <div class="flex-1">
-          <p class="uppercase text-gray-500 md:mb-6">Social</p>
-          <ul class="list-reset mb-6">
-            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-              <a
-                href="#"
-                class="no-underline hover:underline text-gray-800 hover:text-orange-500"
-                >Facebook</a
-              >
-            </li>
-            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-              <a
-                href="#"
-                class="no-underline hover:underline text-gray-800 hover:text-orange-500"
-                >Linkedin</a
-              >
-            </li>
-            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-              <a
-                href="#"
-                class="no-underline hover:underline text-gray-800 hover:text-orange-500"
-                >Twitter</a
-              >
-            </li>
-          </ul>
-        </div>
-        <div class="flex-1">
-          <p class="uppercase text-gray-500 md:mb-6">Company</p>
-          <ul class="list-reset mb-6">
-            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-              <a
-                href="#"
-                class="no-underline hover:underline text-gray-800 hover:text-orange-500"
-                >Official Blog</a
-              >
-            </li>
-            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-              <a
-                href="#"
-                class="no-underline hover:underline text-gray-800 hover:text-orange-500"
-                >About Us</a
-              >
-            </li>
-            <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-              <a
-                href="#"
-                class="no-underline hover:underline text-gray-800 hover:text-orange-500"
-                >Contact</a
-              >
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
+        <v-spacer></v-spacer>
 
-    <a href="https://www.freepik.com/free-photos-vectors/background"
-      >Background vector created by freepik - www.freepik.com</a
-    >
-  </footer>
+        <v-btn href="https://twitter.com/nollan94" class="mx-4" dark icon title="twitter">
+          <v-icon size="24px">mdi-twitter</v-icon>
+        </v-btn>
+        <v-btn href="https://github.com/DavidCouronne" class="mx-4" dark icon title="github">
+          <v-icon size="24px">mdi-github-circle</v-icon>
+        </v-btn>
+      </v-card-title>
+
+      <v-card-text class="py-2 white--text text-center">
+        {{ new Date().getFullYear() }} —
+        <strong>David Couronné</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
 </template>
