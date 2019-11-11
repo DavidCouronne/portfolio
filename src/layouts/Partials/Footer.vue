d498389538c51b6712ee2e5801999a77321789be
David Couronné <couronne.david@gmail.com>
2019-11-11 20:25:07 +0100

    chore(): add legacy

--- src/layouts/Partials/Footer.vue
+++ src/layouts/Partials/Footer.vue
@@ -1,9 +1,10 @@
 <template>
   <v-footer dark padless>
-    <v-card class="flex" flat tile color="black">
-      <v-card-title>
-        <strong class="subheading">Get connected with me!</strong>
-
+    <v-card class="flex" flat tile color="#121212">
+     <v-card-list>
+        
+<v-card-list-item>
+  <g-link to="/Legacy">Legacy</g-link>
         <v-spacer></v-spacer>
 
         <v-btn href="https://twitter.com/nollan94" class="mx-4" dark icon title="twitter">
@@ -12,8 +13,10 @@
         <v-btn href="https://github.com/DavidCouronne" class="mx-4" dark icon title="github">
           <v-icon size="24px">mdi-github-circle</v-icon>
         </v-btn>
-      </v-card-title>
+     </v-card-list-item>
 
+  
+</v-card-list>
       <v-card-text class="py-2 white--text text-center">
         {{ new Date().getFullYear() }} —
         <strong>David Couronné</strong>
